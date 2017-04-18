1df7d124ffb948e6101eda4395d11826c1258cc5
Amey <apotnis@equalexperts.com>
2017-04-18 17:21:32 +0530

    adding test frameworks

--- react-mobx/src/App.test.js
+++ react-mobx/src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+const wrapper = shallow(<App />);
+
+it('should display Welcome to React message ', () => {
+  expect(wrapper.find("#header").text()).to.equal("Welcome to React");
 });
